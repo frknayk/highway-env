3b102da51dc5725a05a2f6c876ad9ae2f3cdcbac
frknayk <furkanayik@outlook.com>
2021-10-31 15:14:39 +0300

    acc env updates

--- highway_env/envs/acc_env.py
+++ highway_env/envs/acc_env.py
@@ -38,7 +38,6 @@ class AccTwoLanesEnv(AbstractEnv):
     def _create_road(self, length=800) -> None:
         """Create a road composed of straight adjacent lanes."""
         net = RoadNetwork()
-
         # Lanes
         net.add_lane("a", "b", StraightLane([0, 0], [length, 0],
                                             line_types=(LineType.CONTINUOUS_LINE, LineType.STRIPED)))
@@ -46,42 +45,48 @@ class AccTwoLanesEnv(AbstractEnv):
                                             line_types=(LineType.NONE, LineType.CONTINUOUS_LINE)))
         net.add_lane("b", "a", StraightLane([length, 0], [0, 0],
                                             line_types=(LineType.NONE, LineType.NONE)))
-
         road = Road(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
         self.road = road
 
-    def _create_vehicles(self) -> None:
+    def _spawn_ego_vehicle(self) -> None:
         """
-        Populate a road with several vehicles on the road
-
-        :return: the ego-vehicle
+        Spawn ego vehicle - SUT: System under test
         """
-        road = self.road
-        ego_vehicle = self.action_type.vehicle_class(road,
-                                                     road.network.get_lane(("a", "b", 1)).position(0, 0),
+        self.vehicle_ego = self.action_type.vehicle_class(self.road,
+                                                     self.road.network.get_lane(("a", "b", 1)).position(0, 0),
                                                      speed=5)
-        road.vehicles.append(ego_vehicle)
-        self.vehicle = ego_vehicle
+        self.road.vehicles.append(self.vehicle_ego)
 
+    def _spawn_agents(self) -> None:
+        """
+        Spawn agent vehicles controlled by AI
+        """
         vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
-        for i in range(3):
-            self.road.vehicles.append(
-                vehicles_type(road,
-                              position=road.network.get_lane(("a", "b", 1))
-                              .position(70+40*i + 10*self.np_random.randn(), 0),
-                              heading=road.network.get_lane(("a", "b", 1)).heading_at(70+40*i),
-                              speed=24 + 2*self.np_random.randn(),
-                              enable_lane_change=False)
-            )
-        for i in range(2):
-            v = vehicles_type(road,
-                              position=road.network.get_lane(("b", "a", 0))
-                              .position(200+100*i + 10*self.np_random.randn(), 0),
-                              heading=road.network.get_lane(("b", "a", 0)).heading_at(200+100*i),
-                              speed=20 + 5*self.np_random.randn(),
-                              enable_lane_change=False)
-            v.target_lane_index = ("b", "a", 0)
-            self.road.vehicles.append(v)
+        vehicles_type(self.road,
+                        position=self.road.network.get_lane(("a", "b", 1))
+                        .position(70+40*i + 10*self.np_random.randn(), 0),
+                        heading=self.road.network.get_lane(("a", "b", 1)).heading_at(70+40*i),
+                        speed=24 + 2*self.np_random.randn(),
+                        enable_lane_change=False)
+
+        # for i in range(3):
+        #     self.road.vehicles.append(
+        #         vehicles_type(self.road,
+        #                       position=self.road.network.get_lane(("a", "b", 1))
+        #                       .position(70+40*i + 10*self.np_random.randn(), 0),
+        #                       heading=self.road.network.get_lane(("a", "b", 1)).heading_at(70+40*i),
+        #                       speed=24 + 2*self.np_random.randn(),
+        #                       enable_lane_change=False)
+        #     )
+        # for i in range(2):
+        #     v = vehicles_type(self.road,
+        #                       position=self.road.network.get_lane(("b", "a", 0))
+        #                       .position(200+100*i + 10*self.np_random.randn(), 0),
+        #                       heading=self.road.network.get_lane(("b", "a", 0)).heading_at(200+100*i),
+        #                       speed=20 + 5*self.np_random.randn(),
+        #                       enable_lane_change=False)
+        #     v.target_lane_index = ("b", "a", 0)
+        #     self.road.vehicles.append(v)
 
     def _reward(self, action: int) -> float:
         """
@@ -92,15 +97,16 @@ class AccTwoLanesEnv(AbstractEnv):
         return 0.0
 
     def _is_terminal(self) -> bool:
-        """The episode is over if the ego vehicle crashed or the time is out."""
-        # return self.vehicle.crashed or \
-        #     self.steps >= self.config["duration"] or \
-        #     (self.config["offroad_terminal"] and not self.vehicle.on_road)
-        return self.vehicle.crashed or self.steps >= self.config["duration"]
+        """
+        The episode is over if the ego vehicle crashed or the time is out.
+        """
+        return self.vehicle_ego.crashed or self.steps >= self.config["duration"]
 
     def _cost(self, action: int) -> float:
-        """The cost signal is the occurrence of collision."""
-        return float(self.vehicle.crashed)
+        """
+        The cost signal is the occurrence of collision.
+        """
+        return float(self.vehicle_ego.crashed)
 
 
 register(
